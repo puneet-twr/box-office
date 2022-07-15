d62fb4ac6b32030e3e8210374860b463097a716e
puneet-twr <pt.twr2@gmail.com>
2022-07-15 18:34:15 +0530

    Added blank pages for react router (Home and starred)

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
+import Navs from "./components/Navs";
+import Home from "./pages/Home";
+import Starred from "./pages/Starred";
 function App() {
   return (
+    <div>
+    <Navs/>   
     <Switch>
-      <Route exact path="/">This is home page</Route>
+      <Route exact path="/"><Home/></Route>
 
-      <Route exact path="/starred">This is starred page</Route>
+      <Route exact path="/starred">
+        <Starred/>
+      </Route>
 
       <Route>404 not found</Route>
     </Switch>
+    </div>
   );
 }
 
