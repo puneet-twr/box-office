580ac2e9dd097ea1909d832f7ff5da0308d8cc5e
puneet-twr <pt.twr2@gmail.com>
2022-07-15 10:21:01 +0530

    Add routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from "react";
-
+import { Switch, Route } from "react-router-dom";
 function App() {
   return (
-    <div className="App">
-      hi
-    </div>
+    <Switch>
+      <Route exact path="/">This is home page</Route>
+
+      <Route exact path="/starred">This is starred page</Route>
+
+      <Route>404 not found</Route>
+    </Switch>
   );
 }
 
