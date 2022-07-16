4a22cb21c2411c27322ebc3e6bf9de9a979d77f4
puneet-twr <pt.twr2@gmail.com>
2022-07-16 09:27:44 +0530

    displayed results and encapsulated fetch logic

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,23 +1,43 @@
 import React from "react";
 import { useState } from "react";
 import MainPageLayout from "../components/MainPageLayout";
-
+import { apiGet } from "../misc/config";
 const Home = () => {
   const [input, setInput] = useState("");
+  const [results, setResults] = useState(null);
   const onInputChange = (e) => {
     setInput(e.target.value);
   };
   const onSearch = () => {
-    fetch(`https://api.tvmaze.com/search/shows?q=${input}`)
-      .then((r) => r.json())
-      .then((result) => {
-        console.log(result);
-      });
+    apiGet(`/search/shows?q=${input}`).then(result=>{
+        setResults(result);
+    })
+    // fetch(`https://api.tvmaze.com/search/shows?q=${input}`)
+    //   .then((r) => r.json())
+    //   .then((result) => {
+    //     setResults(result);
+    //     console.log(result);
+    //   });
   };
   const onKeyDown = (e) => {
     if (e.code === "Enter" || e.code === "NumpadEnter") {
       onSearch();
     }
+  };
+  const renderResults = () => {
+    console.log("Working");
+    if (results && results.length === 0) {
+      return <div>No results</div>;
+    }
+    if (results && results.length > 0) {
+      return (
+        <div>
+          {results.map((item)=>(
+            <div key={item.show.id}>{item.show.name}</div>
+          ))}
+        </div>
+      );
+    } else return null;
   };
   return (
     <MainPageLayout>
@@ -25,6 +45,7 @@ const Home = () => {
       <button type="button" onClick={onSearch}>
         Search
       </button>
+      {renderResults()}
     </MainPageLayout>
   );
 };
