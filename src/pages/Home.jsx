1a85fa14a25fbd51938b1514a70d30b3f3ea2d99
puneet-twr <pt.twr2@gmail.com>
2022-07-16 21:05:43 +0530

    created preview cards

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { useState } from "react";
+import ActorGrid from "../components/actor/ActorGrid";
 import MainPageLayout from "../components/MainPageLayout";
+import ShowGrid from "../components/show/ShowGrid";
 import { apiGet } from "../misc/config";
 const Home = () => {
   const [input, setInput] = useState("");
@@ -30,10 +32,8 @@ const Home = () => {
     }
     if (results && results.length > 0) {
       return results[0].show
-        ? results.map((item) => <div key={item.show.id}>{item.show.name}</div>)
-        : results.map((item) => (
-            <div key={item.person.id}>{item.person.name}</div>
-          ));
+        ? <ShowGrid data={results} />
+        : <ActorGrid data={results} />
     } else return null;
   };
 
