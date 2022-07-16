c58839eee5243a7991b29e2796b0f0fcf3abb790
puneet-twr <pt.twr2@gmail.com>
2022-07-16 08:58:04 +0530

    Added Search bar and fetched the results

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,32 @@
-import React from 'react'
-import MainPageLayout from '../components/MainPageLayout'
+import React from "react";
+import { useState } from "react";
+import MainPageLayout from "../components/MainPageLayout";
 
 const Home = () => {
+  const [input, setInput] = useState("");
+  const onInputChange = (e) => {
+    setInput(e.target.value);
+  };
+  const onSearch = () => {
+    fetch(`https://api.tvmaze.com/search/shows?q=${input}`)
+      .then((r) => r.json())
+      .then((result) => {
+        console.log(result);
+      });
+  };
+  const onKeyDown = (e) => {
+    if (e.code === "Enter" || e.code === "NumpadEnter") {
+      onSearch();
+    }
+  };
   return (
-    <MainPageLayout>This is Home page</MainPageLayout>
-  )
-}
+    <MainPageLayout>
+      <input type="text" onChange={onInputChange} onKeyDown={onKeyDown} />
+      <button type="button" onClick={onSearch}>
+        Search
+      </button>
+    </MainPageLayout>
+  );
+};
 
-export default Home
+export default Home;
